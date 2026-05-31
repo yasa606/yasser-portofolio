957d257061366d893ed9013f3a9de8c72577b59f
jeremiaat <yassermuna01@gmail.com>
2026-05-31 22:59:30 +0300

    vite configure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://yasser-portofolio-orpin.vercel.app/",
+      routes: ["/", "/knowledge-base"],
+    }),
+  ],
 });
