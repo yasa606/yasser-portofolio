b0a565fb6fe56834227fdbb169bb94b490a2fbe3
yasa606 <yassermuna01@gmail.com>
2026-06-01 20:46:23 +0300

    Update canonical link in knowladge_base.jsx

--- src/pages/knowladge base/knowladge_base.jsx
+++ src/pages/knowladge base/knowladge_base.jsx
@@ -342,7 +342,7 @@ const KnowledgeBase = () => {
         />
         <link
           rel="canonical"
-          href="https://yasser-portofolio-orpin.vercel.app/knowledge-base"
+          href="https://yasserportofolio.netlify.app//knowledge-base"
         />
       </Helmet>
 
