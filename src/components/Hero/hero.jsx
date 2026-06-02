2e5e54a3f5ea66b6a7260c268dd0c9778175949e
yasa606 <yassermuna01@gmail.com>
2026-06-02 11:57:43 +0300

    Update profile image source in Hero component

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../assets/profile.png";
+import profile from "../../assets/yasser-profile-photo.png";
 
 const Hero = () => {
   // Helper to handle scrolling
